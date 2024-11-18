fbc10fd8a6d080ec433241470475b059da462afe
sdazaa <sdazaarandia@gmail.com>
2024-11-19 00:53:06 +0100

    Changes after another testing round

--- tests/testthat/test-run_app.R
+++ tests/testthat/test-run_app.R
@@ -1,5 +1,7 @@
+library(shinytest2)
+
 test_that("run_app launches without errors", {
-  expect_silent({
-    shiny::testServer(run_app())
-  })
+  app <- AppDriver$new(run_app, name = "run_app_test", height = 800, width = 1200)
+  expect_true(!is.null(app))
+  app$stop()
 })
