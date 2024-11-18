a3b4e961e3313a8663dafcc2dfa389a2086d73f0
sdazaa <sdazaarandia@gmail.com>
2024-11-19 00:58:12 +0100

    Changes after testing

---
+++ inst/shinyapp/app.R
@@ -0,0 +1,3 @@
+library(bpartysdazaa)
+
+run_app()
--- tests/testthat/test-run_app.R
+++ tests/testthat/test-run_app.R
@@ -1,7 +1,7 @@
 library(shinytest2)
 
 test_that("run_app launches without errors", {
-  app <- AppDriver$new(run_app, name = "run_app_test", height = 800, width = 1200)
+  app <- AppDriver$new(test_path("inst/shinyapp/"), name = "run_app_test", height = 800, width = 1200)
   expect_true(!is.null(app))
   app$stop()
 })
