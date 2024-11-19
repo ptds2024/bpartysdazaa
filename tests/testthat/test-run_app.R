ada048215abb378b002a32215cbf53a7b15687fb
sdazaa <sdazaarandia@gmail.com>
2024-11-19 01:00:38 +0100

    More changes after more testing

--- tests/testthat/test-run_app.R
+++ tests/testthat/test-run_app.R
@@ -1,7 +1,8 @@
 library(shinytest2)
 
 test_that("run_app launches without errors", {
-  app <- AppDriver$new(test_path("inst/shinyapp/"), name = "run_app_test", height = 800, width = 1200)
+  app_dir <- system.file("shinyapp", package = "bpartysdazaa")
+  app <- AppDriver$new(app_dir, name = "run_app_test", height = 800, width = 1200)
   expect_true(!is.null(app))
   app$stop()
 })
