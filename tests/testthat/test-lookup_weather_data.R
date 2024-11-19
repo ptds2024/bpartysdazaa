d3bdc982bec22cd0545fbd3d17f95b1c09447d81
sdazaa <sdazaarandia@gmail.com>
2024-11-19 11:30:16 +0100

    Changes to correct errors and notes

--- tests/testthat/test-lookup_weather_data.R
+++ tests/testthat/test-lookup_weather_data.R
@@ -1,18 +1,20 @@
 library(testthat)
+library(httptest2)
 
 test_that("lookup_weather_data works", {
-  httptest2::with_mock_api({
+  with_mock_api({
     mock_valid <- list(
       cod = "200",
-      list = data.frame(temp = c(10, 12, 15), humidity = c(0.8, 0.7)),
+      list = data.frame(temp = c(10, 12, 15), humidity = c(0.8, 0.7, 0.9)),
       city = list(name = "Lausanne")
     )
-    httptest2::capture_requests({
+
+    capture_requests({
       expect_named(lookup_weather_data("Lausanne"), c("cod", "list", "city"))
     })
 
-    httptest2::capture_requests({
-      expect_error(lookup_weather_data("InvalidCity"), "Failed to retrieve weather data for")
+    capture_requests({
+      expect_error(lookup_weather_data("InvalidCity"), "Failed to retrieve data")
     })
   })
 })
